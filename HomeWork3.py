49092700b87c47646ed7067ee743c23d8bb6b44f
Rashid20071601 <153351715+Rashid20071601@users.noreply.github.com>
2024-09-17 07:29:22 +0300

    Add files via upload

---
+++ HomeWork3.py
@@ -0,0 +1,15 @@
+# 1
+name = 'Rashid'
+print(f'Name: {name}')
+
+# 2
+age = 17
+print(f'Age: {age}')
+
+# 3
+age = age + 1
+print(f'New age: {age}')
+
+# 4
+is_student = True
+print(f'Is student: {is_student}')
